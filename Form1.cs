9b4d5952029d87a08ea8e80666c27529fb2287c8
Emmanuel Adrianne Zabala <emmanueladrianneazabala@iskolarngbayan.pup.edu.ph>
2022-07-29 16:22:46 +0800

    coded json to pdf

--- Form1.cs
+++ Form1.cs
@@ -23,7 +23,57 @@ using iText.Layout.Properties;
 namespace Resume_Generator_Program
 {
 
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class About
+    {
+        public string? summary { get; set; }
+    }
 
+    public class Basics
+    {
+        public string? name { get; set; }
+        public string? email { get; set; }
+        public string? phone { get; set; }
+        public string? title { get; set; }
+        public string? website { get; set; }
+        public string? address { get; set; }
+    }
+
+    public class Education
+    {
+        public string? institution { get; set; }
+        public string? area { get; set; }
+        public string? studyType { get; set; }
+        public string? startDate { get; set; }
+        public string? endDate { get; set; }
+    }
+
+    public class Root
+    {
+        public About? about { get; set; }
+        public Basics? basics { get; set; }
+        public Work? work { get; set; }
+        public Education? education { get; set; }
+        public List<Skill>? skills { get; set; }
+    }
+
+    public class Skill
+    {
+        public string? name { get; set; }
+        public string? level { get; set; }
+        public List<string>? keywords { get; set; }
+    }
+
+    public class Work
+    {
+        public string? company { get; set; }
+        public string? position { get; set; }
+        public string? website { get; set; }
+        public string? startDate { get; set; }
+        public string? endDate { get; set; }
+        public string? summary { get; set; }
+        public List<string>? highlights { get; set; }
+    }
 
 
 
